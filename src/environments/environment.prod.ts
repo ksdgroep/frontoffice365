6f9a71ca176ea61e9c5fc563971fc330fcdac9a5
Bas Goedemans <bas.goedemans@ksdgroep.nl>
2017-06-27 16:37:17 +0200

    KSD Acc-Settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://basic-api.viper365.nl',
-  apiKey: 'A20EC0A0-F5F2-4499-96B0-10370A8E7854',
+  apiUrl: 'https://basic-api-acc.viper365.nl',
+  apiKey: '0329CFA5-5470-409C-89B0-7F31D1A113E7',
   contactEmail: 'info@ksdgroep.nl'
 };
