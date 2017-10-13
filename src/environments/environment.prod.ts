77f7c7d67c5b75c9652cc2aa8feb2f9262bf04a9
Bas Goedemans <bas.goedemans@ksdgroep.nl>
2017-10-13 18:29:01 +0200

    Wellant Settings to Config-File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://basic-api-acc.viper365.nl',
-  apiKey: '85E594E8-E912-4F78-9C80-1B447DC90231',
-  contactEmail: 'info@ksdgroep.nl'
 };
