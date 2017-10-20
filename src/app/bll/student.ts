59846775063a1e37103744f62eb92e8a5f96ff59
Bas Goedemans <bas.goedemans@ksdgroep.nl>
2017-10-20 15:37:20 +0200

    Fieldvalidation Student updated

--- src/app/bll/student.ts
+++ src/app/bll/student.ts
@@ -8,10 +8,13 @@ export class Student {
   PlaceOfBirth: string;
   Email: string;
   Phone: string;
+  PhoneWork: string;
+  Mobile: string;
   Address: string;
   AddressNumber: string;
   PostalCode: string;
   City: string;
   CountryId: string;
   CourseId: number;
+  Function: string;
 }
