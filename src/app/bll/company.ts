0b0623e0886a3c7b3c8bb539d14f621d290068f0
Bas Goedemans <bas.goedemans@ksdgroep.nl>
2017-10-21 15:59:16 +0200

    Company Phone added.

--- src/app/bll/company.ts
+++ src/app/bll/company.ts
@@ -1,9 +1,11 @@
 export class Company {
-    Name: string;
-    Address: string;
-    AddressNumber: string;
-    PostalCode: string;
-    City: string;
-    CountryId: string;
-    VATNumber: string;
+  Name: string;
+  Address: string;
+  AddressNumber: string;
+  PostalCode: string;
+  City: string;
+  CountryId: string;
+  VATNumber: string;
+  Phone: string;
+  Email: string;
 }
